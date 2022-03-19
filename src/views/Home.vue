b088a00cd77531a4567a9ad5b2c40eb47b8592fb
Khin Myat Noe Oo <noenoecu1741995@gmail.com>
2022-03-19 20:31:28 +0630

    vuetify testing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,111 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-text-field
+      v-model="newTaskTilte"
+      @click:append="addTask()"
+      @keyup.enter="addTask()"
+      class="pa-3"
+      outlined
+      label="Add Task"
+      append-icon="mdi-plus"
+      hide-details
+      clearable
+    ></v-text-field>
+    <v-list v-if="tasks.length" class="pt-0" flat>
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item @click="doneTask(task.id)" :class="{ 'blue lighten-5' : task.done }">
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="task.done"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+              :class="{ 'text-decoration-line-through' : task.done }">
+                {{ task.title }}
+              </v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn 
+              @click.stop = "deleteTask(task.id)"
+              icon>
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
+    <div v-else class="no-tasks">
+      <v-icon
+        size="100"
+        color="primary"
+      >
+      mdi-check
+      </v-icon>
+      <div class="text-h5 primary--text">No Task</div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { reactive } from 'vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+        newTaskTilte: '',
+        tasks:[
+          // {
+          //   id: '1',
+          //   title: 'Wake up',
+          //   done: false
+          // },
+          // {
+          //   id: '2',
+          //   title: 'Get bananas',
+          //   done: false
+          // },
+          // {
+          //   id: '3',
+          //   title: 'Eat bananas',
+          //   done: false
+          // }
+        ]
+    }
+  },
+  methods: {
+    addTask() {
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTilte,
+        done: false
+      }
+      this.tasks.push(newTask)
+      this.newTaskTilte = ''
+    },
+    doneTask(id) {
+      let task = this.tasks.filter(task => task.id === id)[0]
+      task.done = !task.done
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id)
+    }
   }
 }
 </script>
+
+<style lang="sass">
+  .no-tasks
+    position: absolute,
+    left: 50%
+    top: 50%
+    transform: translate(-50%, -50%)
+  
+</style>
